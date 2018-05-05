92a7d671ce9fa1c26e2a0f51c9421de46f3f2d76
Taylor Williams <taylor@thewilliams.net>
2018-05-05 18:57:53 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,9 +1,9 @@
 class TicTacToe
-  
+
   def initialize
     board = Array.new(9, " ")
   end
-  
+
   WIN_COMBINATIONS = [
     [0,1,2],
     [3,4,5],
@@ -14,7 +14,7 @@ class TicTacToe
     [0,4,8],
     [6,4,2]
   ]
-  
+
   def display_board(board)
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts "-----------"
@@ -22,23 +22,23 @@ class TicTacToe
     puts "-----------"
     puts " #{board[6]} | #{board[7]} | #{board[8]} "
   end
-  
+
   def input_to_index(user_input)
     user_input.to_i - 1
   end
-  
+
   def move(board, index, token)
     board[index] = token
   end
-  
+
   def position_taken?(board, index)
     !(board[index].nil? || board[index] == " ")
   end
-  
+
   def valid_move?(board, index)
     index.between?(0,8) && !position_taken?(board,index)
   end
-  
+
   def turn(board)
     puts "please enter 1-9:"
     if valid_move?(board, index = input_to_index(gets.strip))
@@ -48,7 +48,7 @@ class TicTacToe
       turn(board)
     end
   end
-  
+
   def turn_count(board)
     turns = 0
     board.each do |token|
@@ -58,11 +58,11 @@ class TicTacToe
     end
     turns
   end
-  
+
   def current_player(board)
     turn_count(board) % 2 == 0 ? "X" : "O"
   end
-  
+
   def won?(board)
     WIN_COMBINATIONS.detect do |combo|
       board[combo[0]] == board[combo[1]] &&
@@ -70,25 +70,25 @@ class TicTacToe
       position_taken?(board, combo[0])
     end
   end
-  
+
   def full?(board)
     board.all? {|player| player == "X" || player == "O"}
   end
-  
+
   def draw?(board)
     full?(board) && !won?(board)
   end
-  
+
   def over?(board)
     draw?(board) || won?(board)
   end
-  
+
   def winner(board)
     if combo = won?(board)
       board[combo.first]
     end
   end
-  
+
   def play(board)
     until over?(board)
       turn(board)
